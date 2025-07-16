b286113affa4b04be5d02748e5a5aeafabc6d50b
Unmystic <unmystic7@gmail.com>
2025-07-16 09:55:18 +0200

    fixed image overflow issue

--- frontend_g/src/App.jsx
+++ frontend_g/src/App.jsx
@@ -4,24 +4,24 @@ import WelcomeScreen from "./components/WelcomeScreen/WelcomeScreen";
 import GameScreen from "./components/GameScreen/GameScreen";
 
 function App() {
-    const [isGameStarted, setIsGameStarted] = useState(false);
+  const [isGameStarted, setIsGameStarted] = useState(false);
 
-    const startGame = () => {
-        setIsGameStarted(true);
-    };
+  const startGame = () => {
+    setIsGameStarted(true);
+  };
 
-    return (
-        <div className="App">
-            {!isGameStarted ? (
-                <WelcomeScreen onStartGame={startGame} />
-            ) : (
-                <GameScreen
-                    imageUrl="/images/1_polar-bear.jpeg"
-                    target={{ name: "Polar Bear" }}
-                />
-            )}
-        </div>
-    );
+  return (
+    <div className="App">
+      {!isGameStarted ? (
+        <WelcomeScreen onStartGame={startGame} />
+      ) : (
+        <GameScreen
+          imageUrl="/images/1_polar-bear2.jpeg"
+          target={{ name: "Polar Bear" }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
